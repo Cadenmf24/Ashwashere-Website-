13114c252edfe0955c29bbca5dfea7ee69e74207
cadenmf24 <cadenmf24@gmail.com>
2023-11-08 15:33:35 -0500

    using react this time

---
+++ websitetest/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to the website of Ash Franklin!</h1>
+    </div>
+  );
+}
+
+export default App;
