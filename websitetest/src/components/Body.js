47c2b32898aeefaa3df7a17567ccd84805d949a9
cadenmf24 <cadenmf24@gmail.com>
2023-11-08 23:53:41 -0500

    it does a thing

--- websitetest/src/components/Body.js
+++ websitetest/src/components/Body.js
@@ -17,8 +17,7 @@ const Body = () => {
 
             I am a nerd, I know.
             </p>
-            
-                <img className = "App-image" src = "https://cdn.discordapp.com/attachments/876921481672654929/1171996841672445962/IMG_4868.jpg?ex=655eb61d&is=654c411d&hm=ac7f18481b59b4eab6fa3622ea51f60ff81b966c6e0dd668ffef8a3e1a397a25&"></img>
+            <img className = "App-image" src = "https://cdn.discordapp.com/attachments/876921481672654929/1171996841672445962/IMG_4868.jpg?ex=655eb61d&is=654c411d&hm=ac7f18481b59b4eab6fa3622ea51f60ff81b966c6e0dd668ffef8a3e1a397a25&"></img>
         </div>
         </div>
 
