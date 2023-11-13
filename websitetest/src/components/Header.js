c90c689ad73e417baa939fb5ce400d85cc64d9dd
cadenmf24 <cadenmf24@gmail.com>
2023-11-13 10:06:38 -0500

    Under development

--- websitetest/src/components/Header.js
+++ websitetest/src/components/Header.js
@@ -10,7 +10,8 @@ import Aboutme from "../sites/Aboutme";
 const Header = () => {
     return(
         <div className="App-header">
-            <h1> Welcome To the Website of Ash Franklin</h1>
+            <h1> Welcome To the Website of Ash Franklin </h1>
+            <h1>THIS WEBSITE IS STILL VERY MUCH UNDER DEVELOPEMENT, CHECK GITHUB FOR MORE INFOMATION github.com/cadenmf24</h1>
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
             </style>
